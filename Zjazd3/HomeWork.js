b1827a3ac5d6659bf6fc1f558d413be9eec7afa9
adam-rzonca <adam.rzonca@gmail.com>
2019-11-03 17:22:57 +0100

    Cała praca domowa

--- Zjazd3/HomeWork.js
+++ Zjazd3/HomeWork.js
@@ -138,3 +138,64 @@
 // function div(x, y) {
 //   return x / y;
 // }
+
+// 3) Create an array (by hand) of objects and call sum() function in context of each one of them.
+// Sum function is to come from this object  BaseObject = { X,y, sum: function (){ return this.x+this.y}}
+
+// Example array: [{x:2,y:3},{x:-1,x:6,{x:0,x:8},…..]
+
+// let BaseObject = {
+//   X: 0,
+//   y: 0,
+//   sum: function() {
+//     return this.x + this.y;
+//   }
+// };
+
+// let myArray = [
+//   { x: 2, y: 3 },
+//   { x: -1, y: 6 },
+//   { x: 0, y: 8 },
+//   { x: 5, y: -7 }
+// ];
+
+// for (let i = 0; i < myArray.length; i++) {
+//   let sumFun = BaseObject.sum.bind(myArray[i]);
+//   console.log(sumFun());
+// }
+
+// // 4) Given an array of objects, for each object call operation() function in context of next object.
+// // If object is last, got back to start of the array for operation function. In loop;
+// let myArray = [
+//   {
+//     x: 1,
+//     y: "object one value",
+//     //operation1: () => "Object one prefix " + this.x + " " + this.y,
+//     operation: function() {
+//       return "Object one prefix " + this.x + " " + this.y;
+//     }
+//   },
+//   {
+//     x: 2,
+//     y: "object two value",
+//     //operation1: () => "Object two prefix " + this.x + " " + this.y,
+//     operation: function() {
+//       return "Object two prefix " + this.x + " " + this.y;
+//     }
+//   },
+//   {
+//     x: 3,
+//     y: "object three value",
+//     //operation1: () => "Object three prefix " + this.x + " " + this.y,
+//     operation: function() {
+//       return "Object three prefix " + this.x + " " + this.y;
+//     }
+//   }
+// ];
+
+// for (let i = 0; i < myArray.length; i++) {
+//   let nextIndex = (i + 1) % 3;
+
+//   let resultOperation = myArray[i].operation.bind(myArray[nextIndex]);
+//   console.log(resultOperation());
+// }
