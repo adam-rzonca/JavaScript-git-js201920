d1a69308aca80942203ddf4ec58c8a71714ceee0
Adam Rzońca <adam.rzonca@gmail.com>
2020-03-10 21:38:41 +0100

    Zadanie 3

---
+++ Lab7/Zad3.js
@@ -0,0 +1,54 @@
+// 3) Given two strings, write a program that efficiently finds the longest common subsequence (substring).
+// 'karol rolki'
+
+"use strict";
+
+var utils = (function() {
+  function genString(length) {
+    let result = new Array(length).fill("");
+
+    result = result.map(elem => (Math.random() < 0.5 ? "A" : "B"));
+    return result.join("");
+  }
+  return { genString };
+})();
+
+class longestCommonSubstring {
+  constructor(str1, str2) {
+    this.str1 = str1;
+    this.str2 = str2;
+  }
+
+  find() {
+    if (this.str1.length === 0 || this.str2.length === 0) {
+      return "";
+    }
+
+    let result = this.str1.charAt(0);
+
+    for (let i = 0; i < this.str1.length; i++) {
+      for (let j = i; j < this.str1.length; j++) {
+        let substr = this.str1.substr(i, j + 1);
+
+        if (substr.length > result.length && this.str2.includes(substr)) {
+          result = substr;
+        } else {
+          break;
+        }
+      }
+    }
+
+    return result;
+  }
+}
+
+const strLen = 10;
+const str1 = utils.genString(strLen);
+const str2 = utils.genString(strLen);
+
+const lcs = new longestCommonSubstring(str1, str2);
+const result = lcs.find();
+
+console.log(str1);
+console.log(str2);
+console.log(result);
