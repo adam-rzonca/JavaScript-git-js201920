ce1b388fa810476df1b91e9024990b68ee766979
Adam Rzońca <adam.rzonca@gmail.com>
2020-04-23 23:38:30 +0200

    Dodałem tworzenie tablic ruchu figur

--- Lab8/Task2/ChessBoardGame.js
+++ Lab8/Task2/ChessBoardGame.js
@@ -7,25 +7,60 @@ const chessPieceTypes = require("./CheesePieceTypes.js");
 
 module.exports = class ChessBoardGame {
   constructor() {
-    this.board = []; // Jednowymiarowa tablica zawierająca WOLNE pola na planszy szachowej
+    this.freeFields = []; // Jednowymiarowa tablica zawierająca WOLNE pola na planszy szachowej
+    this.chessPieces = []; // Tablica wylosowanych figur szachowych. Zawiere obiekty typu ChessPiece
+    this.chessPieceFactory = new ChessPieceFactory();
 
     // Wypełnienie tablicy 64 elementami zawierajacymi kolejne wspólrzędne pól planszy szachowej
     for (let i = 1; i <= 8; i++) {
       for (let j = 1; j <= 8; j++) {
-        this.board.push(new Position(i, j));
+        this.freeFields.push(new Position(i, j));
       }
     }
   }
 
   play() {
-    while (this.board.length) {
-      const index = utils.randomInt(0, this.board.length); // Wylosowanie indeksu wolnego pola
-      const position = this.board.splice(index, 1); // Usunięcie wylosowanego pola z tablicy wolnych pól
+    const board = [
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+    ];
+    console.table(board);
 
-      const chessPieceType = utils.randomInt(0, chessPieceTypes.length); // Wylosowanie typu figury
+    // Dopóki mamy WOLNE pola na planszy
+    while (this.freeFields.length) {
+      const randomChessPiece = this.createRandomChessPiece();
+      this.chessPieces.push(randomChessPiece);
 
-      const piece = chessPieceTypes[chessPieceType];
-      console.log(piece, position.toString());
+      board[randomChessPiece.position.x - 1][
+        randomChessPiece.position.y - 1
+      ] = randomChessPiece.toString();
     }
+
+    console.table(board);
+  }
+
+  createRandomChessPiece() {
+    let index = utils.randomInt(0, this.freeFields.length); // Wylosowanie indeksu wolnego pola
+    const [randomPosition] = this.freeFields.splice(index, 1); // Usunięcie wylosowanego pola z tablicy wolnych pól
+
+    index = utils.randomInt(0, chessPieceTypes.length); // Wylosowanie typu figury
+    //index = 0; // Król
+    //index = 1; // Królowa
+    //index = 2; // Wieża
+    //index = 3; // Goniec
+    //index = 4; // Koń
+    const randomType = chessPieceTypes[index];
+
+    const options = { type: randomType, position: randomPosition };
+
+    console.log(options.type, options.position.toString());
+
+    return this.chessPieceFactory.createChessPiece(options);
   }
 };
--- Lab8/Task2/ChessPieceFactory.js
+++ Lab8/Task2/ChessPieceFactory.js
@@ -1,22 +1,274 @@
 "use strict";
-
+const Position = require("./Position.js");
 const chessPieceTypes = require("./CheesePieceTypes.js");
+
+class ChessPiece {
+  constructor(position) {
+    this.position = position;
+    this.moveOptions = [];
+  }
+
+  rowAndColumnMoves() {
+    const result = [];
+
+    for (let i = 1; i <= 8; i++) {
+      if (this.position.x !== i) {
+        result.push(new Position(i, this.position.y));
+      }
+    }
+
+    for (let i = 1; i <= 8; i++) {
+      if (this.position.y !== i) {
+        result.push(new Position(this.position.x, i));
+      }
+    }
+
+    return result;
+  }
+
+  diagonalMoves() {
+    let result = [];
+
+    // W lewo w dół
+    for (
+      let i = this.position.x - 1, j = this.position.y - 1;
+      i >= 1 && j >= 1;
+      i--, j--
+    ) {
+      result.push(new Position(i, j));
+    }
+
+    // W prawo w górę
+    for (
+      let i = this.position.x + 1, j = this.position.y + 1;
+      i <= 8 && j <= 8;
+      i++, j++
+    ) {
+      result.push(new Position(i, j));
+    }
+
+    // W lewo w górę
+    for (
+      let i = this.position.x - 1, j = this.position.y + 1;
+      i >= 1 && j <= 8;
+      i--, j++
+    ) {
+      result.push(new Position(i, j));
+    }
+
+    // W prawo w dół
+    for (
+      let i = this.position.x + 1, j = this.position.y - 1;
+      i <= 8 && j >= 1;
+      i++, j--
+    ) {
+      result.push(new Position(i, j));
+    }
+    return result;
+  }
+
+  toString() {
+    if (this instanceof King) {
+      return "\u2654";
+    } else if (this instanceof Queen) {
+      return "\u2655";
+    } else if (this instanceof Rook) {
+      return "\u2656";
+    } else if (this instanceof Bishop) {
+      return "\u2657";
+    } else if (this instanceof Knight) {
+      return "\u2658";
+    } else {
+      return "?";
+    }
+  }
+}
+
+class King extends ChessPiece {
+  constructor(position) {
+    super(position);
+    super.moveOptions = this.setMoveOptions();
+  }
+
+  setMoveOptions() {
+    const result = [];
+    let position;
+
+    if (this.position.x - 1 >= 1) {
+      // W lewo w górę
+      if (this.position.y + 1 <= 8) {
+        position = new Position(this.position.x - 1, this.position.y + 1);
+        result.push(position);
+      }
+
+      // W lewo
+      position = new Position(this.position.x - 1, this.position.y);
+      result.push(position);
+
+      // W lewo w dół
+      if (this.position.y - 1 >= 1) {
+        position = new Position(this.position.x - 1, this.position.y - 1);
+        result.push(position);
+      }
+    }
+
+    // W górę
+    if (this.position.y + 1 <= 8) {
+      position = new Position(this.position.x, this.position.y + 1);
+      result.push(position);
+    }
+
+    // W dół
+    if (this.position.y - 1 >= 1) {
+      position = new Position(this.position.x, this.position.y - 1);
+      result.push(position);
+    }
+
+    if (this.position.x + 1 <= 8) {
+      // W prawo w górę
+      if (this.position.y + 1 <= 8) {
+        position = new Position(this.position.x + 1, this.position.y + 1);
+        result.push(position);
+      }
+
+      // W prawo
+      position = new Position(this.position.x + 1, this.position.y);
+      result.push(position);
+
+      // W prawo w dół
+      if (this.position.y - 1 >= 1) {
+        position = new Position(this.position.x + 1, this.position.y - 1);
+        result.push(position);
+      }
+    }
+
+    return result;
+  }
+}
+
+class Queen extends ChessPiece {
+  constructor(position) {
+    super(position);
+    super.moveOptions = this.setMoveOptions();
+  }
+
+  setMoveOptions() {
+    return this.rowAndColumnMoves().concat(this.diagonalMoves());
+  }
+}
+
+// Wieża
+class Rook extends ChessPiece {
+  constructor(position) {
+    super(position);
+    super.moveOptions = this.setMoveOptions();
+  }
+
+  setMoveOptions() {
+    return this.rowAndColumnMoves();
+  }
+}
+
+// Goniec
+class Bishop extends ChessPiece {
+  constructor(position) {
+    super(position);
+    super.moveOptions = this.setMoveOptions();
+  }
+
+  setMoveOptions() {
+    return this.diagonalMoves();
+  }
+}
+
+// Koń
+class Knight extends ChessPiece {
+  constructor(position) {
+    super(position);
+    super.moveOptions = this.setMoveOptions();
+  }
+
+  setMoveOptions() {
+    const result = [];
+    let position;
+
+    // W górę
+    if (this.position.y + 2 <= 8) {
+      //  i w lewo
+      if (this.position.x - 1 >= 1) {
+        position = new Position(this.position.x - 1, this.position.y + 2);
+        result.push(position);
+      }
+
+      //  i w prawo
+      if (this.position.x + 1 <= 8) {
+        position = new Position(this.position.x + 1, this.position.y + 2);
+        result.push(position);
+      }
+    }
+
+    // W lewo
+    if (this.position.x - 2 >= 1) {
+      // i w górę
+      if (this.position.y + 1 <= 8) {
+        position = new Position(this.position.x - 2, this.position.y + 1);
+        result.push(position);
+      }
+      // i w dół
+      if (this.position.y - 1 >= 1) {
+        position = new Position(this.position.x - 2, this.position.y - 1);
+        result.push(position);
+      }
+    }
+
+    // W dół
+    if (this.position.y - 2 >= 1) {
+      //  i w lewo
+      if (this.position.x - 1 >= 1) {
+        position = new Position(this.position.x - 1, this.position.y - 2);
+        result.push(position);
+      }
+
+      //  i w prawo
+      if (this.position.x + 1 <= 8) {
+        position = new Position(this.position.x + 1, this.position.y - 2);
+        result.push(position);
+      }
+    }
+
+    // W prawo
+    if (this.position.x + 2 <= 8) {
+      // i w górę
+      if (this.position.y + 1 <= 8) {
+        position = new Position(this.position.x + 2, this.position.y + 1);
+        result.push(position);
+      }
+      // i w dół
+      if (this.position.y - 1 >= 1) {
+        position = new Position(this.position.x + 2, this.position.y - 1);
+        result.push(position);
+      }
+    }
+
+    return result;
+  }
+}
 
 module.exports = class ChessPieceFactory {
   constructor() {}
 
   createChessPiece(options) {
     switch (options.type) {
-      case "king":
-        break;
-      case "queen":
-        break;
-      case "rook": // Wieża
-        break;
-      case "bishop": // Goniec
-        break;
-      case "knight": // Koń
-        break;
+      case chessPieceTypes[0]: // Król
+        return new King(options.position);
+      case chessPieceTypes[1]: // Królowa
+        return new Queen(options.position);
+      case chessPieceTypes[2]: // Wieża
+        return new Rook(options.position);
+      case chessPieceTypes[3]: // Goniec
+        return new Bishop(options.position);
+      case chessPieceTypes[4]: // Koń
+        return new Knight(options.position);
       default:
         throw new Error("Invalid chess piece type!");
     }
